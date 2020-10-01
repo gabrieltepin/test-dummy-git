48ae4bdf503890b183294bc8cf7af216c2e4d093
gabrieltepin <gabrieltepin@gmail.com>
2020-10-01 02:54:29 -0300

    agora esta em dia com a master

--- app/src/components/HelloWorld.vue
+++ app/src/components/HelloWorld.vue
@@ -6,7 +6,30 @@
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
+<<<<<<< HEAD
     <h1> some text </h1>
+=======
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+    <p>
+      For a guide and recipes on how to configure / customize this project,<br>
+      check out the
+      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+    </p>
+>>>>>>> origin
   </div>
 </template>
 
